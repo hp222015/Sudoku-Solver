cd6ee03681bc4c709aff56f939865c072f7985a8
Harshit Pawar <97798536+hp222015@users.noreply.github.com>
2023-09-12 22:56:16 +0530

    Update sudokusolve.cpp

--- sudokusolve.cpp
+++ sudokusolve.cpp
@@ -1,159 +1,152 @@
-#include <bits/stdc++.h>
+// Basic and Easy CPP Sudoku Solver
+
+/* Ready to use Code
+
+can be compiled using online or offline compilers */
+
+#include <iostream>
+#include<stdio.h>
 using namespace std;
- 
-// UNASSIGNED is used for empty
-// cells in sudoku grid
-#define UNASSIGNED 0
- 
-// N is used for the size of Sudoku grid.
-// Size will be NxN
-#define N 9
- 
-// This function finds an entry in grid
-// that is still unassigned
-bool FindUnassignedLocation(int grid[N][N],
-                            int& row, int& col);
- 
-// Checks whether it will be legal
-// to assign num to the given row, col
-bool isSafe(int grid[N][N], int row,
-            int col, int num);
- 
-/* Takes a partially filled-in grid and attempts
-to assign values to all unassigned locations in
-such a way to meet the requirements for
-Sudoku solution (non-duplication across rows,
-columns, and boxes) */
-bool SolveSudoku(int grid[N][N])
+
+#define N 9 //defining grid dimension, that is 9 by 9
+int sudokuboard[N][N];
+
+/*  If you don't want to give the input of 81 numbers, 
+you may always use the commented out pre-defined sudoku arrangement in a 2-D array 
+or you can edit the code and always pre-define by yourself for your use. 
+Thank You! */
+
+
+/*int sudokuboard[N][N] = 
 {
-    int row, col;
- 
-    // If there is no unassigned location,
-    // we are done
-    if (!FindUnassignedLocation(grid, row, col))
-        // success!
-        return true;
- 
-    // Consider digits 1 to 9
-    for (int num = 1; num <= 9; num++)
-    {
-         
-        // Check if looks promising
-        if (isSafe(grid, row, col, num))
-        {
-             
-            // Make tentative assignment
-            grid[row][col] = num;
- 
-            // Return, if success
-            if (SolveSudoku(grid))
-                return true;
- 
-            // Failure, unmake & try again
-            grid[row][col] = UNASSIGNED;
-        }
+ {0, 0, 6, 5, 0, 8, 4, 0, 0},
+ {5, 2, 0, 0, 0, 1, 0, 0, 0},
+ {0, 7, 8, 0, 0, 0, 0, 0, 1},
+ {0, 0, 4, 0, 1, 0, 0, 8, 0},
+ {9, 0, 0, 8, 2, 3, 0, 0, 5},
+ {0, 5, 0, 0, 9, 0, 6, 0, 0},
+ {1, 3, 0, 0, 0, 0, 2, 5, 0},
+ {0, 0, 0, 0, 0, 0, 0, 7, 4},
+ {0, 0, 5, 2, 0, 6, 3, 0, 0}
+};*/
+
+
+/* The checkColumn() function takes the column number 
+and the input number or value as its arguments 
+and check if the number already exists in the specified column or not*/
+
+
+
+bool checkColumn(int c, int value)
+{ 
+ for (int r = 0; r < N; r++)
+  if (sudokuboard[r][c] == value)
+   return true;
+ return false;
+}
+
+/*  The checkRow() function matches the input number with every number in the specified row 
+and returns true or false after verifying its presence */
+
+
+bool checkRow(int r, int value)
+{ 
+for (int c = 0; c < N; c++)
+ if (sudokuboard[r][c] == value)
+  return true;
+ return false;
+}
+
+/* */
+
+bool checkBox(int initial_row, int initial_column, int value)
+{
+for (int r = 0; r < 3; r++)
+ for (int c = 0; c < 3; c++)
+  if (sudokuboard[r+initial_row][c+initial_column] == value)
+   return true;
+ return false;
+}
+
+/* showGame() function to print the sudoku board on the console screen */
+
+
+void showGame()
+{ 
+for (int r = 0; r < N; r++){
+ for (int c = 0; c < N; c++)
+ {
+  if(c == 3 || c == 6)
+   cout << " | ";
+  cout << sudokuboard[r][c]<<" ";
+ }
+ if(r == 2 || r == 5)
+ {
+  cout << endl;
+  for(int k = 0; k<N; k++)
+   cout << "---";
+ }
+ cout << endl;
+}
+}
+
+/* The findBox() function looks for an empty box on the entire game board to see if the user has completed the game or not */
+
+
+
+bool findBox(int &r, int &c)
+{ 
+ for (r = 0; r < N; r++)
+  for (c = 0; c < N; c++)
+   if (sudokuboard[r][c] == 0) 
+    return true;
+ return false;
+}
+
+
+/* The function canEnter() takes the input and calls these above functions inside its body. 
+It checks the output they return*/
+
+
+
+
+bool canEnter(int r, int c, int value)
+{
+ return !checkRow(r, value) && !checkColumn(c, value) && !checkBox(r-r%3,c-c%3,value);
+}
+
+/* */
+
+bool finalSolution(){
+int r, c;
+if (!findBox(r, c))
+ return true; 
+for (int val = 1; val <= 9; val++)
+ { 
+  if (canEnter(r, c, val))
+  { 
+   sudokuboard[r][c] = val;
+   if (finalSolution()) 
+    return true;
+   sudokuboard[r][c] = 0; 
+  }
+ }
+return false;
+}
+
+/* The main() function, the user only calls the finalSolution(), which is the primary function that solves the sudoku game */
+
+
+int main(){
+    cout<<"Enter your 81 numbers (0 <= Input_Numbers <= 9) :"<<endl;
+    int sudokuboard[N][N];
+    for(int i=0;i<9;i++){
+    for(int j=0;j<9;j++){
+        cin>>sudokuboard[i][j];
     }
-    
-    // This triggers backtracking
-    return false;
 }
- 
-/* Searches the grid to find an entry that is
-still unassigned. If found, the reference
-parameters row, col will be set the location
-that is unassigned, and true is returned.
-If no unassigned entries remain, false is returned. */
-bool FindUnassignedLocation(int grid[N][N],
-                            int& row, int& col)
-{
-    for (row = 0; row < N; row++)
-        for (col = 0; col < N; col++)
-            if (grid[row][col] == UNASSIGNED)
-                return true;
-    return false;
-}
- 
-/* Returns a boolean which indicates whether
-an assigned entry in the specified row matches
-the given number. */
-bool UsedInRow(int grid[N][N], int row, int num)
-{
-    for (int col = 0; col < N; col++)
-        if (grid[row][col] == num)
-            return true;
-    return false;
-}
- 
-/* Returns a boolean which indicates whether
-an assigned entry in the specified column
-matches the given number. */
-bool UsedInCol(int grid[N][N], int col, int num)
-{
-    for (int row = 0; row < N; row++)
-        if (grid[row][col] == num)
-            return true;
-    return false;
-}
- 
-/* Returns a boolean which indicates whether
-an assigned entry within the specified 3x3 box
-matches the given number. */
-bool UsedInBox(int grid[N][N], int boxStartRow,
-               int boxStartCol, int num)
-{
-    for (int row = 0; row < 3; row++)
-        for (int col = 0; col < 3; col++)
-            if (grid[row + boxStartRow]
-                    [col + boxStartCol] ==
-                                       num)
-                return true;
-    return false;
-}
- 
-/* Returns a boolean which indicates whether
-it will be legal to assign num to the given
-row, col location. */
-bool isSafe(int grid[N][N], int row,
-            int col, int num)
-{
-    /* Check if 'num' is not already placed in
-    current row, current column
-    and current 3x3 box */
-    return !UsedInRow(grid, row, num)
-           && !UsedInCol(grid, col, num)
-           && !UsedInBox(grid, row - row % 3,
-                         col - col % 3, num)
-           && grid[row][col] == UNASSIGNED;
-}
- 
-/* A utility function to print grid */
-void printGrid(int grid[N][N])
-{
-    for (int row = 0; row < N; row++)
-    {
-        for (int col = 0; col < N; col++)
-            cout << grid[row][col] << " ";
-        cout << endl;
-    }
-}
- 
-// Driver Code
-int main()
-{
-    // 0 means unassigned cells
-    int grid[N][N] = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
-                       { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
-                       { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
-                       { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
-                       { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
-                       { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
-                       { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
-    if (SolveSudoku(grid) == true)
-        printGrid(grid);
-    else
-        cout << "No solution exists";
- 
-    return 0;
+if (finalSolution() == true)
+ showGame();
+else
+ cout << "This game can not be solved";
 }
